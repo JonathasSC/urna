260385e189d2d00108dc2bc49438baeaa5316e35
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-11-05 20:49:41 -0300

    starting features

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 11111,
+                name: 'João Tapa',
+                partido: 'PSTP',
+                fotos: [
+                    {url:'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/135x145/24bpp/9302_operario.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: 22222,
+                name: 'Vitor Pedrada',
+                partido: 'PSPS',
+                fotos: [
+                    {url:'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/135x145/24bpp/95_boto.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: 33333,
+                name: 'Anderson Lapada',
+                partido: 'VSLP',
+                fotos: [
+                    {url:'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/95x105/24bpp/95_boitata.jpg', legenda: 'Vereador', small: fals }
+                ]
+            },
+            {
+                numero: 44444,
+                name: 'Joana Machadada',
+                partido: 'PLPT',
+                fotos: [
+                    {url:'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/135x145/24bpp/93_professora.jpg', legenda: 'Vereador', small: fals }
+                ]
+            },
+            {
+                numero: 55555,
+                name: 'Vilson Mancada',
+                partido: 'LSDP',
+                fotos: [
+                    {url:'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/95x105/24bpp/93_magico.jpg', legenda: 'Vereador', small: fals }
+                ]
+            },
+        ]
+    }
+]
