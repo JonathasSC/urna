ffe0baf9e7ba6a2332f7911fab7dc5ad441e9394
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-11-08 20:00:53 -0300

    começando logica

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ let descricao = document.querySelector('.d-1-4')
 let aviso = document.querySelector('.d-2')
 let lateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0;
+
+function ComecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    SeuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = "";
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml
+}
+
+function clicou(n){
+    alert("clicou em "+n)
+}
+
+function branco(){
+    alert("clicou em BRANCO!")
+}
+
+function corrige(){
+    alert("clicou em CORRIGE!")
+}
+
+function confirma(){
+    alert("clicou em CONFIRMA!")
+}
+
+ComecarEtapa()
